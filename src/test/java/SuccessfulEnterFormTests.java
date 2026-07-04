3f0b73fcd603d636f6bd7e2e789ac166fdfa543d
denor1999 <ozhgixinv@list.ru>
2026-07-04 15:07:39 +0300

    create successful enter form with only required fields

--- src/test/java/SuccessfulEnterFormTests.java
+++ src/test/java/SuccessfulEnterFormTests.java
@@ -3,21 +3,26 @@ import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.sleep;
 
-public class SuccessfulEnterForm {
+public class SuccessfulEnterFormTests {
 
     @BeforeAll
-    static void startup(){
+    static void startup() {
+        Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
     }
 
     @Test
     void successEnterFormTest(){
-        Selenide.open("https://demoqa.com/automation-practice-form");
+        Selenide.open("/automation-practice-form");
         $("[id=firstName]").setValue("Ivan");
         $("[id=lastName]").setValue("Ozhgikhin");
         $("[value=Male]").click();
         $("input[id=userNumber]").setValue("1234567890");
+        $("#submit").scrollTo().click();
     }
 }
